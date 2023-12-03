462b928a552b611b3c579e62cae95285a7b3fb84
D3v1s0m <d3v1s0m@gmail.com>
2023-12-03 22:53:18 +0530

    home page, added color scale, logo

--- index.js
+++ index.js
@@ -12,15 +12,30 @@ app.use(compression());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  console.log(`${new Date().toLocaleTimeString()}: ${req.method} ${req.url}`);
+  next();
+});
+
+const views = [
+  { name: 'World Map', description: 'A world map showing the population of each country', url: '/world-map', title: 'World Map' },
+  { name: 'World Map 3D', description: 'A 3D world map showing the population of each country', url: '/world-map-3d', title: 'World Map 3D' },
+  { name: 'Female %', description: 'A world map showing the female population percentage of each country', url: '/gender-per', title: 'Female Population Percentage' },
+];
+
 app.get('/', (req, res) => {
-  res.render('main', { view: '' });
+  res.render('main', { views, title: 'TimoAtlas - Home' });
+});
+
+app.get('/world-map', (req, res) => {
+  res.render('main', { view: 'worldMapView', title: 'World Map' });
 });
 
 app.get('/gender-per', (req, res) => {
-  res.render('main', { view: 'genderPercView' });
+  res.render('main', { view: 'genderPercView', title: 'Female Population Percentage' });
 });
 
-app.get('/3d', (req, res) => {
+app.get('/world-map-3d', (req, res) => {
   res.render('main', { view: 'worldMapView3D' });
 });
 
